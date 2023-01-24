d67a5dc916fb7f0cfbe81e0cf55f204955935e56
srothfleisch <spencerrothfleisch2024@u.northwestern.edu>
2023-01-23 22:01:29 -0600

    completed part I

--- robot.py
+++ robot.py
@@ -4,12 +4,14 @@ import pybullet as p
 import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import constants as c
+from pyrosim.neuralNetwork import NEURAL_NETWORK
 
 class ROBOT:
     def __init__(self):
         self.motor = dict()
         self.sensors = dict()
         self.robotId = p.loadURDF("body.urdf", [0, 0, 0.1])
+        self.nn = NEURAL_NETWORK("brain.nndf")
         pyrosim.Prepare_To_Simulate(self.robotId)
         self.Prepare_To_Sense()
         self.Prepare_To_Act()
@@ -31,6 +33,14 @@ class ROBOT:
         pass
 
     def Act(self, x):
-        for jointName in pyrosim.jointNamesToIndices:
-            self.motor[jointName].Set_Value(self.robotId, x)
+        for neuronName in self.nn.Get_Neuron_Names():
+            if self.nn.Is_Motor_Neuron(neuronName):
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+                desiredAngle = self.nn.Get_Value_Of(neuronName)
+                self.motor[jointName].Set_Value(self.robotId, desiredAngle)
+        pass
+
+    def Think(self):
+        self.nn.Update()
+        self.nn.Print()
         pass
